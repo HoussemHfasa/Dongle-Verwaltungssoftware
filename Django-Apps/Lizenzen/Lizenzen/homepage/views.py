b99827dc5fa399ac26dec8a511f0670f65a3ccd4
rama <ramaabazeed654@gmail.com>
2023-05-18 21:25:21 +0200

    rest_framework-Bibliothek installieret

--- Django-Apps/Lizenzen/Lizenzen/homepage/views.py
+++ Django-Apps/Lizenzen/Lizenzen/homepage/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from .models import MyModel
 from .serializers import MyModelSerializer
 # Create your views here.
+
 from django.http import HttpResponse
 
 def home(request):
