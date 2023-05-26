ba1ad0da868d03daf32c69395f09a813ca4a71e1
rama <ramaabazeed654@gmail.com>
2023-05-26 17:11:29 +0200

    license_managment wieder hergestellt

--- Backend/homepage/models.py
+++ Backend/homepage/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Dongle(models.Model):
-    lfd_nr_field = models.IntegerField(db_column='Lfd. Nr.', primary_key=True)  # Field name made lowercase. Field renamed to remove unsuitable characters. Field renamed because it ended with '_'. The composite primary key (Lfd. Nr., Benutzer_Firmcode) found, that is not supported. The first column is selected.
-    serien_nr = models.CharField(db_column='Serien-Nr', max_length=45)  # Field name made lowercase. Field renamed to remove unsuitable characters.
-    name = models.CharField(db_column='Name', max_length=45)  # Field name made lowercase.
+    lfd_nr_field = models.IntegerField(db_column='Lfd. Nr.', primary_key=True)
+    serien_nr = models.CharField(db_column='Serien-Nr', max_length=45)
+    name = models.CharField(db_column='Name', max_length=45)
     
-    benutzer_firmcode = models.IntegerField(db_column='Benutzer_Firmcode')  # Field name made lowercase.
+    benutzer_firmcode = models.IntegerField(db_column='Benutzer_Firmcode')
 
     class Meta:
         managed = False
