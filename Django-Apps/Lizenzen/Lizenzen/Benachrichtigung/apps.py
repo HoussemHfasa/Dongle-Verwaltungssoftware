0f16daeddef25ebc051f728682e956fcda987db5
Houssem_Hfasa <houssem.hfasa@student.htw-berlin.de>
2023-05-28 23:16:48 +0200

    Unnötige Dateien gelöscht und README bearbeitet

--- Django-Apps/Lizenzen/Lizenzen/Benachrichtigung/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BenachrichtigungConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Benachrichtigung'
--- Django-Apps/Lizenzen/Lizenzen/DongleAnfordern/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from ..models import Ticket, Dognle
-
-class TicketSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Ticket
-        fields = '__all__'
-
-        
