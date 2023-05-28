e3de52d714c4a7226f982aa9ba9384b35d26f7a2
Houssem_Hfasa <houssem.hfasa@student.htw-berlin.de>
2023-05-28 16:44:25 +0200

    Datenbank bearbeitet

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -9,17 +9,10 @@ urlpatterns = [
     # path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('User_loggin/', include('User_loggin.urls')),
     path("", include("homepage.urls")),
-<<<<<<< HEAD
     path("", include("benachrichtigung.urls")),
     path("", include("Lizenzseite.urls")),
-
-]
-=======
-    path('user_loggin/', include('User_loggin.urls')),
     path('', include('User_loggin.urls')),  
     path("api/login", UserLoginAPIView.as_view(), name="api-login"),
     ]
     
->>>>>>> Houssem
