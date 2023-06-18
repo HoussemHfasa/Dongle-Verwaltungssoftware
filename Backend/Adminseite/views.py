a3ed1420fe6f814240091a98765fa43b1e58828c
Houssem_Hfasa <houssem.hfasa@student.htw-berlin.de>
2023-06-18 15:01:47 +0200

    Kommentare zum Backend hinzufügen und Code bereinigen

--- Backend/Adminseite/views.py
+++ Backend/Adminseite/views.py
@@ -5,37 +5,37 @@ from rest_framework import status
 from .models import Customuser
 from .serializers import CustomuserSerializer
 
-
 class CustomuserView(APIView):
-    def get(self, request, *args, **kwargs):
-        result = Customuser.objects.all()
-        serializer = CustomuserSerializer(result, many=True)
-        return Response({'status': 'success', 'data': serializer.data}, status=200)
-
-    def post(self, request, *args, **kwargs):
-        serializer = CustomuserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'status': 'success', 'data': serializer.data}, status=201)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def put(self, request, pk, *args, **kwargs):
-        instance = get_object_or_404(Customuser, pk=pk)
-        serializer = CustomuserSerializer(instance, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'status': 'success', 'data': serializer.data}, status=200)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, *args, **kwargs):
-     instance = get_object_or_404(Customuser, pk=pk)
  
-     # Check if the user has is_superuser set to 1
-     if instance.is_superuser == 1:
-        return Response(
-            {'status': 'error', 'message': 'Cannot delete an admin account with superuser privileges.'},
-            status=status.HTTP_400_BAD_REQUEST
-        )
+ def get(self, request, *args, **kwargs):   #Alle Benutzer abrufen
+    result = Customuser.objects.all()
+    serializer = CustomuserSerializer(result, many=True)
+    return Response({'status': 'success', 'data': serializer.data}, status=200)   
 
-     instance.delete()
-     return Response({'status': 'success', 'message': 'Deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+ def post(self, request, *args, **kwargs):  #Benutzer erstellen
+    serializer = CustomuserSerializer(data=request.data)
+    if serializer.is_valid():  
+        serializer.save()  
+        return Response({'status': 'success', 'data': serializer.data}, status=201)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  
+
+ def put(self, request, pk, *args, **kwargs):   #Benutzer aktualisieren
+    instance = get_object_or_404(Customuser, pk=pk)
+    serializer = CustomuserSerializer(instance, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({'status': 'success', 'data': serializer.data}, status=200)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+ def delete(self, request, pk, *args, **kwargs):  #Benutzer löschen
+    instance = get_object_or_404(Customuser, pk=pk)
+ 
+    # Überprüfen, ob beim Benutzer is_superuser auf 1 gesetzt ist
+    if instance.is_superuser == 1:
+     return Response(
+        {'status': 'error', 'message': 'Cannot delete an admin account with superuser privileges.'},  
+        status=status.HTTP_400_BAD_REQUEST
+    )  
+
+    instance.delete()
+    return Response({'status': 'success', 'message': 'Deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
